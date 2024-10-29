8bd7c005d84c39379a4110b5c3b2d13f7be56ece
Codey Gallup <codey.gallup@gmail.com>
2024-10-29 14:17:52 -0600

    Passing all tests

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,30 @@
 // import the Media class:
-
+const Media = require("./Media");
 // create your Book class:
+class Book extends Media {
+  constructor(title, year, genre, author, numPages, rating) {
+    super(title, year, genre);
+    this.author = author;
+    this.numPages = numPages;
+    this.rating = rating;
+  }
+
+  static highestRating(arr) {
+    let highestRate = 0;
+    let highestBook;
+    for (let book of arr) {
+      if (book.rating > highestRate) {
+        highestRate = book.rating;
+        highestBook = book;
+      }
+    }
+    return highestBook;
+  }
+
+  summary() {
+    return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+}
 
 // don't change below
 module.exports = Book;
--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,24 @@
 // import the Media class:
+const Media = require("./Media");
 
 // create your Music class:
+class Music extends Media {
+  constructor(title, year, genre, artist, length) {
+    super(title, year, genre);
+    this.artist = artist;
+    this.length = length;
+  }
+
+  static shortestAlbum(arr) {
+    return arr.reduce((shortest, music) =>
+      music.length < shortest?.length ? music : shortest
+    );
+  }
+
+  summary() {
+    return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`;
+  }
+}
 
 // don't change below
 module.exports = Music;
